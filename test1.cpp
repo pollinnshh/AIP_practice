9c9a1dfa336bdf88be48e4fcf4a8500b0b687c4b
pollinnshh <polinashvch248@gmail.com>
2025-12-02 09:44:37 +0300

    t1

---
+++ test1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+namespace topit {
+  struct p_t {
+    int x, y;
+  };
+# include <iostream>
+namespace topit {
+  struct p_t {
+      int x, y;
+};
+struct f_t {
+    p_t aa, bb;
+};
+  bool operator==(p_t a, p_t b);
+  bool operator!=(p_t a, p_t b);
+}
+int main() {
+    using namespace topit;
+    p_t a{1, 1}, b{0, 1};
+    std::cout << (a == b) << "\n";
+}
+bool topit::operator==(p_t a, p_t b) {
+    return a.x == b.x && a.y == b.y;
+}
+bool topit::operator!=(p_t a, p_t b) {
+    return !(a == b);
+}
+
