cffdb600f4f8381ffada30a0c89361608b66ddbc
pollinnshh <polinashvch248@gmail.com>
2025-12-02 09:56:06 +0300

    struct IDraw done

--- test1.cpp
+++ test1.cpp
@@ -13,11 +13,25 @@ struct IDraw {
     virtual p_t begin() const = 0;
     virtual p_t next(p_t prev) const = 0;
 };
+struct Dot: IDraw {
+    p_t begin() const override;
+    p_t next(p_t prev) const override;
+    p_t d;
+};
 }
 int main() {
     using namespace topit;
     p_t a{1, 1}, b{0, 1};
     std::cout << (a == b) << "\n";
+}
+topit::p_t topit::Dot::begin() const {
+    return d;
+}
+topit::p_t topit::Dot::next(p_t prev) const {
+    if (prev != d) {
+        throw std::logic_error("bad prev");
+    }
+    return d;
 }
 bool topit::operator==(p_t a, p_t b) {
     return a.x == b.x && a.y == b.y;
