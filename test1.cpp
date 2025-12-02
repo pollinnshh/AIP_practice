76a9b622c0791905405c7761d1c5f4aa4454c2ee
pollinnshh <polinashvch248@gmail.com>
2025-12-02 12:12:53 +0300

    extend/frame/canvas/paint)

--- test1.cpp
+++ test1.cpp
@@ -1,13 +1,16 @@
 # include <iostream>
 namespace topit {
-  struct p_t {
+struct p_t {
       int x, y;
 };
 struct f_t {
     p_t aa, bb;
 };
-  bool operator==(p_t a, p_t b);
-  bool operator!=(p_t a, p_t b);
+size_t rows(f_t fr);
+size_ t cols(f_t fr):
+
+bool operator==(p_t a, p_t b);
+bool operator!=(p_t a, p_t b);
 struct IDraw {
     virtual ~IDraw() = default;
     virtual p_t begin() const = 0;
@@ -19,6 +22,8 @@ struct Dot: IDraw {
     p_t next(p_t prev) const override;
     p_t d;
 };
+p_t * extend(const p_t * pts, size_t s, p_t fill);
+void extend(p_t ** pts, size_t & s, p_t fill);
 void append(const IDraw * sh, p_t ** ppts, size_t & s);
 f_t frame(const p_t * pts, size_t);
 char * canvas(f_t fr);
@@ -27,6 +32,7 @@ void flush(std::ostream & os, const char * cnv, f_t fr);
 }
 int main() {
     using namespace topit;
+    int err = 0;
     IDraw * shp[3] = {};
     p_t * pts = nullptr;
     size_t s = 0;
@@ -52,7 +58,41 @@ int main() {
     delete shp[2];
     return err;
 }
-
+topit::p_t * topit::extend(const p_t * pts, size_t s, p_t fill) {
+    p_t * r = new p_t[s + 1];
+    for (size_t i = 0; i < s; ++i) {
+        r[i] = pts[i];
+    }
+    r[s] = fill;
+    return r;
+}
+void topit::extend(p_t ** pts, size_t & s, p_t fill) {
+    p_t * r = extend(*pts, s, fill);
+    delete [] *pts;
+    ++s;
+    *pts = r;
+}
+void topit::paint(p_t p, char * cnv, f_t fr, char fill) {
+    size_t dx = p.x - fr.aa.x;
+    size_t dy = fr.bb.y - p.y;
+    cnv[dy * cols(fr) + dx] = fill;
+}
+void topit::flush(std::ostream & os, const char * cnv, f_t fr) {
+    for (size_t i = 0; i < rows(fr); ++i) {
+        for (size_t j = 0; j < cols(fr); ++j) {
+            os << cnv[i * cols(fr) + j];
+        }
+        os << "\n";
+    }
+}
+char * topit::canvas(f_t fr, char fill) {
+    size_t s = rows(fr) * cols(fr);
+    char * c = new char[s];
+    for (size_t i = 0; i < s; ++i) {
+        c[i] = fill;
+    }
+    return c;
+}
 topit::f_t topit::frame(const p_t * pts, size_t s) {
     int minx = pts[0].x;
     int miny = pts[0].y;
@@ -70,6 +110,7 @@ topit::f_t topit::frame(const p_t * pts, size_t s) {
 }
 
 topit::Dot::Dot(p_t dd):
+ IDraw(),
  d{dd}
 {}
 topit::p_t topit::Dot::begin() const {
@@ -80,6 +121,12 @@ topit::p_t topit::Dot::next(p_t prev) const {
         throw std::logic_error("bad prev");
     }
     return d;
+}
+size_t topit::rows(f_t fr) {
+    return (fr.bb.y - fr.aa.y + 1);
+}
+size_t topit::cols(f_t fr) {
+    return (fr.bb.x - fr.aa.x + 1);
 }
 bool topit::operator==(p_t a, p_t b) {
     return a.x == b.x && a.y == b.y;
