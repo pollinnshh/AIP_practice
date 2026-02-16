2bcd3fb9ed4d26b7eaaf72e01963f737fb372387
pollinnshh <polinashvch248@gmail.com>
2026-02-16 12:32:34 +0300

    ex2 rotation

--- 16-02.cpp
+++ 16-02.cpp
@@ -1,7 +1,7 @@
 // 1 вставка (шаблон)
 // добавить в указанную позицию и описать неявный интерфейс
 
-template <class T>
+template< class T >
 T* insert(const T* a, size_t s, size_t i, T v)
 {
     // T::T() - конструктор по умолчанию
@@ -21,4 +21,26 @@ T* insert(const T* a, size_t s, size_t i, T v)
 // плюс ловим исключения try - catch
 
 
+// 2.1 "разворот" массива (шаблон)
+// элемнты должны идти в обратном порядке и описать неявный интерфейс
 
+template< class T >
+T* reverse(const T* a, size_t s)
+{
+    T* r = new T[s];  // T::T()
+    // try {
+    for (size_t k = 0; k < s; ++k)
+    {
+        r[k] = a[s - 1 - k];  // T::operator = (T)
+    }
+    // } catch (...) { delete [] r; throw; }
+    return r;
+}
+
+// 2.2 "разворот" массива там, где он есть
+
+template< class T >
+void reverse(const T* a, size_t s)
+{
+    
+}
