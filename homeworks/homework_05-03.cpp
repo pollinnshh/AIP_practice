39e0f9d955787bde0a68df988fb0ddc0bcf4a95f
pollinnshh <polinashvch248@gmail.com>
2026-03-03 12:35:04 +0300

    cleaning

---
+++ homeworks/homework_05-03.cpp
@@ -0,0 +1,25 @@
+template< typename T > T* merge2(const T* a, size_t sa, const T* b, size_t sb, T* c)
+{
+    size_t i = 0, j = 0, k = 0;
+    while (i < sa && j < sb)
+    {
+        if (a[i] < b[j])
+        {
+            c[k++] = a[i++];
+        } else
+        {
+            c[k++] = b[j++];
+        }
+    }
+    
+    while (i < sa)
+    {
+        c[k++] = a[i++];
+    }
+    while (j < sb)
+    {
+        c[k++] = b[j++];
+    }
+    
+    return c;
+}
